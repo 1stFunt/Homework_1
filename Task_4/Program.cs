f2b7f0c7fae8a930ebfadcf9f72195d81e30461e
Funt <funt_88@mail.ru>
2022-11-12 02:36:02 +0300

    решили Задачу 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите натуральное число (N): ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int number = 1;
+if (a < 0 || a == 0)
+{
+    Console.WriteLine("Ошибка! Введено НЕ натуральное число (N), попробуйте ещё раз.");
+}
+while (number <= a)
+{
+    if (number % 2 == 0)
+    {
+        Console.Write(number + " ");
+        number++;
+    }
+    else
+    {
+        number++;
+    }
+}
